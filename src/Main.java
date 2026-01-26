94b02322dd2d701dbdf21caf9f977d2c8c2f997c
Parneel Bhakhri <hi@pbhak.dev>
2026-01-26 11:16:23 -0800

    add boilerplate and libs

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import core.DisplayWindow;
+
+public class RunMe {
+    public static void main(String[] args) {
+        // --== Load an image to filter ==--
+//        DisplayWindow.showFor("image.jpg", 900, 600, "DoNothingFilter");
+
+        // --== Determine your input interactively with menus ==--
+//        DisplayWindow.getInputInteractively(800,600);
+    }
+}
